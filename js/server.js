74701315b7e30641b73d470bd430220b1e342899
Jordi Corvillo <68904969+jcorvillo@users.noreply.github.com>
2022-05-11 20:27:14 +0200

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,1 +1,11 @@
+const { createReadStream } = require("fs");
+const { createServer } = require("http");
+const { PORT = 8000 } = process.env;
+const HTML_CONTENT_TYPE = "text/html";
+const requestListener = (req, res) => {
+  res.writeHead(200, { "Content-Type": HTML_CONTENT_TYPE });
+  createReadStream("index.html").pipe(res);
+};
+const server = createServer(requestListener);
+server.listen(PORT);
 
